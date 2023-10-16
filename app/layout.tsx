f30d75ec9fa450c7399fc9b20c12f0aea570ba9f
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2023-10-16 21:52:55 +0200

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import './globals.css'
+import './globals.css' 
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
@@ -24,4 +24,4 @@ export default function RootLayout({
       </body>
     </html>
   )
-}  
+}
